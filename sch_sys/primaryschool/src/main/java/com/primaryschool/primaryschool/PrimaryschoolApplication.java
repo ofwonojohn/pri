610457ecee3db052efb5831b8875f2bcb8243770
Ofwonojohn <Ofwonojohn004@gmail.com>
2026-02-18 20:52:20 +0300

    initalized new project for primary school

---
+++ sch_sys/primaryschool/src/main/java/com/primaryschool/primaryschool/PrimaryschoolApplication.java
@@ -0,0 +1,13 @@
+package com.primaryschool.primaryschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaryschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimaryschoolApplication.class, args);
+	}
+
+}
